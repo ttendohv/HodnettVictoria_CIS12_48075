6bbc4b6e5ab566d10d400398145955a7e68d4d55
ttendohv <vhodnett1@student.rcc.edu>
2014-09-04 17:18:22 -0700

    Signed-off-by: ttendohv <vhodnett1@student.rcc.edu>

--- Book/Chapter 2/multi.php
+++ Book/Chapter 2/multi.php
@@ -11,7 +11,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Multidimensional Arrays</title>
+	<title>Multidimensional Arrays</title>
 </head>
 
 <body>
