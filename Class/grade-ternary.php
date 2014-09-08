47c95b4e8ca3a93dbd87b413cf07d8487356e69c
ttendohv <vhodnett1@student.rcc.edu>
2014-09-08 09:18:10 -0700

    Signed-off-by: ttendohv <vhodnett1@student.rcc.edu>

---
+++ Class/grade-ternary.php
@@ -0,0 +1,29 @@
+<?php
+	/*
+		Victoria Hodnett
+		September 8, 2014
+		Purpose: Illustrate Branching Constructs
+	*/
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+	//Initialize the input
+	$score = rand(50,100);
+	//Determine the Grade
+	$grade = ($score>=90)?'A':
+			 (($score>=80)?'B':
+			 (($score>=70)?'C':
+			 (($score>=60)?'D':'F'))); 	
+	//Output the Results
+	echo "<h1>A score of $score = $grade</h1>";
+?>
+</body>
+</html>
---
+++ Class/gradeif.php
@@ -0,0 +1,30 @@
+<?php
+	/*
+		Victoria Hodnett
+		September 8, 2014
+		Purpose: Illustrate Branching Constructs
+	*/
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+	//Initialize the input
+	$score = rand(50,100);
+	//Determine the Grade
+	if($score >= 90) $grade = 'A';
+	if($score >= 80 && $score < 90) $grade = 'B';
+	if($score >= 70 && $score < 80) $grade = 'C';
+	if($score >= 60 && $score < 70) $grade = 'D';
+	if($score < 60) $grade = 'F';
+	//Output the Results
+	echo "<h1>A score of $score = $grade</h1>";
+?>
+</body>
+</html>
